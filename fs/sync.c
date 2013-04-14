9cd6c12c1d3be10a650bfd300ccbaf2efd27c234
Andrew Bartholomew <andrewb03@gmail.com>
2013-04-14 00:16:08 -0500

    fs/dyn_fsync: check dyn fsync control's active prior to performing fsync ops
    Signed-off-by: Andrew Bartholomew <andrewb03@gmail.com>
    
    Conflicts:
    	fs/sync.c

--- fs/sync.c
+++ fs/sync.c
@@ -21,6 +21,7 @@
 #include <trace/events/mmcio.h>
 #ifdef CONFIG_DYNAMIC_FSYNC
 extern bool early_suspend_active;
+extern bool dyn_fsync_active;
 #endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
@@ -190,8 +191,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	int err;
 #ifdef CONFIG_DYNAMIC_FSYNC
-
-	if (!early_suspend_active)
+	if (dyn_fsync_active && !early_suspend_active)
 		return 0;
 	else {
 #endif
@@ -350,7 +350,7 @@ no_async:
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
+	if (dyn_fsync_active && !early_suspend_active)
 		return 0;
 	else
 #endif
@@ -360,7 +360,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
+	if (dyn_fsync_active && !early_suspend_active)
 		return 0;
 	else
 #endif
@@ -435,7 +435,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
+	if (dyn_fsync_active && !early_suspend_active)
 		return 0;
 	else {
 #endif
@@ -539,7 +539,7 @@ SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
+	if (dyn_fsync_active && !early_suspend_active)
 		return 0;
 	else
 #endif
