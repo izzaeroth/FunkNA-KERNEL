7f8cf4ade8de9ca59fad1590cad899247bf8d2e6
flar2 <asegaert@gmail.com>
2013-11-26 09:19:19 -0500

    Add Graphics boost
    Conflicts:
    	arch/arm/mach-msm/include/mach/kgsl.h
    	drivers/cpufreq/cpufreq_ondemand.c
    	drivers/gpu/msm/kgsl_pwrctrl.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -35,6 +35,10 @@
  * It helps to keep variable names smaller, simpler
  */
 
+#include <mach/kgsl.h>
+
+static int old_up_threshold;
+
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
@@ -147,6 +151,7 @@ static struct dbs_tuners {
 	int          powersave_bias;
 	unsigned int io_is_busy;
 	unsigned int input_boost;
+	int gboost;
 } dbs_tuners_ins = {
 	.up_threshold_multi_core = DEF_FREQUENCY_UP_THRESHOLD,
 	.up_threshold = DEF_FREQUENCY_UP_THRESHOLD,
@@ -159,6 +164,7 @@ static struct dbs_tuners {
 	.sync_freq = 0,
 	.optimal_freq = 0,
 	.input_boost = 0,
+	.gboost = 1,
 };
 
 static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
@@ -325,6 +331,7 @@ show_one(optimal_freq, optimal_freq);
 show_one(up_threshold_any_cpu_load, up_threshold_any_cpu_load);
 show_one(sync_freq, sync_freq);
 show_one(input_boost, input_boost);
+show_one(gboost, gboost);
 
 static ssize_t show_powersave_bias
 (struct kobject *kobj, struct attribute *attr, char *buf)
@@ -704,6 +711,19 @@ skip_this_cpu_bypass:
 	return count;
 }
 
+static ssize_t store_gboost(struct kobject *a, struct attribute *b,
+				const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+
+	ret = sscanf(buf, "%u", &input);
+	if(ret != 1)
+		return -EINVAL;
+	dbs_tuners_ins.gboost = (input > 0 ? input : 0);
+	return count;
+}
+
 static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 				const char *buf, size_t count)
 {
@@ -726,6 +746,7 @@ define_one_global_rw(optimal_freq);
 define_one_global_rw(up_threshold_any_cpu_load);
 define_one_global_rw(sync_freq);
 define_one_global_rw(input_boost);
+define_one_global_rw(gboost);
 
 static struct attribute *dbs_attributes[] = {
 	&sampling_rate_min.attr,
@@ -743,6 +764,7 @@ static struct attribute *dbs_attributes[] = {
 	&sync_freq.attr,
 	&input_boost.attr,
 	&boostpulse.attr,
+	&gboost.attr,
 	NULL
 };
 
@@ -903,6 +925,16 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 				dbs_tuners_ins.sampling_down_factor;
 		dbs_freq_increase(policy, policy->max);
 		return;
+	}
+
+//graphics boost
+	if (graphics_boost && dbs_tuners_ins.gboost) {
+		if (dbs_tuners_ins.up_threshold != 49)
+			old_up_threshold = dbs_tuners_ins.up_threshold;
+		dbs_tuners_ins.up_threshold = 49;
+	} else {
+		if (dbs_tuners_ins.up_threshold == 49)
+			dbs_tuners_ins.up_threshold = old_up_threshold;
 	}
 
 	if (num_online_cpus() > 1) {
