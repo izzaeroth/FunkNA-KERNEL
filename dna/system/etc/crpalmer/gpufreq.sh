1294e7dc55b387b76ad7a26aa71e61dcda0459b1
Jamie Morris <jamiethemorris@gmail.com>
2014-02-19 23:18:15 -0800

    only enable gpu script with my kernel

--- dna/system/etc/crpalmer/gpufreq.sh
+++ dna/system/etc/crpalmer/gpufreq.sh
@@ -4,29 +4,33 @@ F=/sdcard/jamiethemorris-gpu-oc
 
 gpufreq=400000000
 
-if [ -r $F ]
+if grep jamiethemorris /proc/version > /dev/null
 then
-    value="`head -1 $F`"
-    if [ "$value" = "450" ]
-    then
-	gpufreq=450000000
-    else
-        if [ "$value" = "500" ]
-        then
-	    gpufreq=500000000
+
+	if [ -r $F ]
+	then
+	    value="`head -1 $F`"
+	    if [ "$value" = "450" ]
+	    then
+		gpufreq=450000000
 	    else
-        	if [ "$value" = "550" ]
-        	then
-		    gpufreq=550000000
-		else
-	    	    gpufreq=400000000
+	        if [ "$value" = "500" ]
+	        then
+		    gpufreq=500000000
+		    else
+	        	if [ "$value" = "550" ]
+	        	then
+			    gpufreq=550000000
+			else
+		    	    gpufreq=400000000
+		    fi
+	    	fi
 	    fi
-    	fi
-    fi
-else
-    gpufreq=400000000
+	else
+	    gpufreq=400000000
+	fi
+
+	echo $gpufreq > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+	
+	echo "setting max GPU frequency to $value MHz"
 fi
-
-echo $gpufreq > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
-
-echo "setting max GPU frequency to $value MHz"
