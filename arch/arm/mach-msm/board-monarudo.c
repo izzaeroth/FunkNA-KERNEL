41a60a4251f0aecf4014417ac35f904b126f1e83
Jamie Morris <jamiethemorris@gmail.com>
2014-03-27 05:12:18 -0700

    disable overclock ifdef for intellithermal

--- arch/arm/mach-msm/board-monarudo.c
+++ arch/arm/mach-msm/board-monarudo.c
@@ -3248,10 +3248,12 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 #ifdef CONFIG_INTELLI_THERMAL
         .poll_ms = 250,
-#ifdef CONFIG_CPU_OVERCLOCK
+/* We are using overclock by default */
+
+/* #ifdef CONFIG_CPU_OVERCLOCK */
         .limit_temp_degC = 70,
-#else
-        .limit_temp_degC = 60,
+/* #else
+        .limit_temp_degC = 60, */
 #endif
         .temp_hysteresis_degC = 10,
         .freq_step = 2,
@@ -3259,10 +3261,10 @@ static struct msm_thermal_data msm_thermal_pdata = {
         .core_limit_temp_degC = 80,
 #else
 	.poll_ms = 1000,
-#ifdef CONFIG_CPU_OVERCLOCK
+/* #ifdef CONFIG_CPU_OVERCLOCK */
 	.limit_temp = 70,
-#else
-	.limit_temp = 60,
+/* #else
+	.limit_temp = 60, */
 #endif
 	.temp_hysteresis = 10,
 	.limit_freq = 918000,
